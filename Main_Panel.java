adff19b960f9b986f038ea03f9312cb96bf94b59
Nazrul Islam <nzl.nadeem@gmail.com>
2015-10-26 10:02:33 +0600

    Create Main_Panel.java

---
+++ Main_Panel.java
@@ -0,0 +1,14 @@
+
+public class Main_Panel {
+
+static {
+
+System.out.println("Test Statci Variable..");
+}
+static final int NUMBER_TEST=300;
+
+	public static void main(String[] args) {
+
+	}
+
+}
